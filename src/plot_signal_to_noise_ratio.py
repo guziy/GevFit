e95fdb141898c36c4f3bd0f40617a101f0d98ae1
Huziy Oleksandr <guziy.sasha@gmail.com>
2011-07-20 16:16:11 -0400

    started investigating signal to noise ratio

--- src/plot_signal_to_noise_ratio.py
+++ src/plot_signal_to_noise_ratio.py
@@ -1,8 +1,15 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
 __author__="huziy"
 __date__ ="$Jul 20, 2011 9:01:26 AM$"
+
+
+
+from calculate_significance_from_bootstrap import get_pars_for_member_and_type
+
+import application_properties
+application_properties.set_current_directory()
+
+
+
 
 if __name__ == "__main__":
     print "Hello World"
